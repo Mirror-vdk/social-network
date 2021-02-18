ac3dd34e8bc72b2794035204cf774ec093ee95a6
Mirror-vdk <prostogeka93@list.ru>
2021-02-19 00:37:26 +1000

    31

--- src/Components/Dialogs/NewMessage/NewMessage.jsx
+++ src/Components/Dialogs/NewMessage/NewMessage.jsx
@@ -2,13 +2,19 @@ import React from "react";
 import s from "./NewMessage.module.css";
 
 const NewMessage = (props) => {
+    let newMessageElement = React.createRef()
+
+    let addMessage = () => {
+        let text = newMessageElement.current.value
+        alert(text)
+    }
     return (
         <div className={s.public_dialogs}>
             <div>
-                <textarea placeholder="Введите сообщение" className={s.dialogs_area}></textarea>
+                <textarea placeholder="Введите сообщение" className={s.dialogs_area} ref={newMessageElement}></textarea>
             </div>
             <div className={s.area_buttons}>
-                <button>Отправить</button>
+                <button onClick={addMessage}>Отправить</button>
             </div>
         </div>
     )
--- src/Components/Profile/MyPosts/MyPosts.jsx
+++ src/Components/Profile/MyPosts/MyPosts.jsx
@@ -7,15 +7,21 @@ const MyPosts = (props) => {
     let postsElements =
         props.posts.map(p => <Post post={p.post} id={p.id} likesCount={p.likesCount} /> )
 
+    let newPostElement = React.createRef()
+
+    let addPost = () => {
+        let text = newPostElement.current.value
+            alert(text)
+    }
     return (
         <section className={s.main}>
                 <div className={s.profile_info}>
                     <div>
                        <h1>Мои посты</h1>
                         <div className={s.Newpost_block}>
-                            <textarea placeholder="Введите текст" className={s.post_area}>1</textarea>
+                            <textarea placeholder="Введите текст" className={s.post_area} ref={newPostElement}>1</textarea>
                             <div className={s.button_area}>
-                                <button className={s.button_post}>Добавить</button>
+                                <button className={s.button_post} onClick={addPost} >Добавить</button>
                             </div>
                             <div className={s.button_area}>
                                 <button className={s.button_post}>Удалить</button>
