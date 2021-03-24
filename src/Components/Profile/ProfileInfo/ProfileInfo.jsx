e1128defb77e481593d1acb917619faeaede10bf
Mirror-vdk <prostogeka93@list.ru>
2021-03-24 23:19:01 +1000

    84

--- src/Components/Profile/ProfileInfo/ProfileInfo.jsx
+++ src/Components/Profile/ProfileInfo/ProfileInfo.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import s from "./ProfileInfo.module.css"
 import Preloader from "../../../assets/Preloader/Preloader";
 import ProfileStatus from './ProfileStatus'
+import ProfileStatusWithHooks from "./ProfileStatusWithHooks";
 
 const ProfileInfo = (props) => {
    if (!props.profile ) {
@@ -12,7 +13,7 @@ const ProfileInfo = (props) => {
         <div className={s.profile_ava}>
             <img src={props.profile.photos.large} className={s.user_avatar}/>
             <div>{props.profile.fullName}</div>
-            <ProfileStatus status={props.status} updateStatus={props.updateStatus}/>
+            <ProfileStatusWithHooks status={props.status} updateStatus={props.updateStatus}/>
         </div>
     )
 }
---
+++ src/Components/Profile/ProfileInfo/ProfileStatusWithHooks.jsx
@@ -0,0 +1,34 @@
+import React, {useState} from "react";
+import s from "./ProfileInfo.module.css"
+
+
+const ProfileStatusWithHooks = (props) => {
+
+    let [editMode, setEditMode] = useState(false)
+    let [status, setStatus] = useState(props.status)
+    const activateEditMode = () => {
+        setEditMode(true)
+    }
+    const deactivateEditMode = () => {
+        setEditMode(false)
+        props.updateStatus(status)
+    }
+    const onStatusChange = (e) => {
+        setStatus(e.currentTarget.value)
+    }
+
+    return (
+        <div>
+            {!editMode &&
+            <div>
+                <span onDoubleClick={activateEditMode}>{props.status || "Статус отсутсвует"}</span>
+            </div>}
+            {editMode &&
+            <textarea onBlur={deactivateEditMode} onChange={onStatusChange} value={status} autoFocus={true}
+                      className={s.status_area}/>
+            }
+        </div>
+    )
+}
+
+export default ProfileStatusWithHooks
